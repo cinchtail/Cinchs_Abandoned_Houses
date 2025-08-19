52306a12ef101014cf74b1700d480e835ced440b
Joshua Davies <cinchtail@gmail.com>
2025-08-19 16:00:49 -0400

    1.5

--- src/main/java/net/cinchtail/cinchsabandonedhouses/CinchsAbandonedHouses.java
+++ src/main/java/net/cinchtail/cinchsabandonedhouses/CinchsAbandonedHouses.java
@@ -5,7 +5,4 @@ import net.minecraftforge.fml.common.Mod;
 @Mod(CinchsAbandonedHouses.MODID)
 public class CinchsAbandonedHouses {
     public static final String MODID = "cinchsabandonedhouses";
-
-    public CinchsAbandonedHouses() {
-    }
 }
