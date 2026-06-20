20ca8df293ec6f568948451d4135106e88a4800e
Joshua Davies <cinchtail@gmail.com>
2026-06-20 19:45:45 -0400

    taiga houses

--- src/main/java/net/cinchtail/cinchsabandonedhouses/CinchsAbandonedHouses.java
+++ src/main/java/net/cinchtail/cinchsabandonedhouses/CinchsAbandonedHouses.java
@@ -1,12 +1,8 @@
 package net.cinchtail.cinchsabandonedhouses;
 
-import org.slf4j.Logger;
-
 import net.neoforged.fml.common.Mod;
-import org.slf4j.LoggerFactory;
 
 @Mod(CinchsAbandonedHouses.MOD_ID)
 public class CinchsAbandonedHouses {
     public static final String MOD_ID = "cinchsabandonedhouses";
-    public static final Logger LOGGER = LoggerFactory.getLogger(CinchsAbandonedHouses.class);
 }
